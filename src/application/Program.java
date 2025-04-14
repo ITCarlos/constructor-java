7cee2274357709b2b4fbea067f817be8c3d33e2e
unknown <carlosafdev@gmail.com>
2025-04-14 00:36:19 -0300

    Repository created

---
+++ src/application/Program.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter product data:");
+		System.out.print("Product name: ");
+		String productName = input.nextLine();
+		System.out.print("Price: $");
+		double price = input.nextDouble();
+		System.out.print("Quantity in stock: ");
+		int quantity = input.nextInt();
+		
+		Product product = new Product(productName, price, quantity);
+		
+		System.out.println();
+		System.out.println("Product data: " + product);
+		
+		System.out.print("\nNumber of products to be added in stock: ");
+		quantity = input.nextInt();
+		product.addQuantity(quantity);
+		System.out.println("Product data: " + product);
+		
+		System.out.print("\nNumber of products to be removed from stock: ");
+		quantity = input.nextInt();
+		product.removeQuantity(quantity);
+		System.out.println("Product data: " + product);
+		
+		input.close();
+	}
+
+}
---
+++ src/entities/Product.java
@@ -0,0 +1,41 @@
+package entities;
+
+public class Product {
+
+	public String name;
+	public double price;
+	public int quantity;
+
+	/* A constructor is used to initialize the attributes
+	 * Allow or force an object to receive data
+	 */
+	
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	public void addQuantity(int quantity) {
+		this.quantity += quantity;
+	}
+	
+	public void removeQuantity(int quantity) {
+		this.quantity -= quantity;
+	}
+	
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+
+	@Override
+	public String toString() {
+		return  name + ", price: $" 
+				+ String.format("%.2f", price) 
+				+ ", quantity: " 
+				+ quantity
+				+ ", Total: $"
+				+ String.format("%.2f", totalValueInStock());
+	}
+	
+}
